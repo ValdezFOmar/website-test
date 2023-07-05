bd8014f717158e06970ffd768accf2dbb1bdf44c
ValdezFOmar <omarantoniovaldezf2@gmail.com>
2023-07-05 11:22:47 -0700

    Refactor main.js code

--- first-site/scripts/main.js
+++ first-site/scripts/main.js
@@ -1,37 +1,41 @@
-const myImage = document.querySelector("img")
-
-//Change image by clicking it
-myImage.onclick = () => {
-    const mySrc = myImage.getAttribute("src")
+changeImage = (image_element) => {
+    const mySrc = image_element.getAttribute("src")
     if (mySrc === "images/reimu.png") {
-        myImage.setAttribute("src", "images/marisa.png")
-        myImage.setAttribute("alt", "Osana Marisa eating.")
+        image_element.setAttribute("src", "images/marisa.png")
+        image_element.setAttribute("alt", "Osana Marisa eating.")
     } else {
-        myImage.setAttribute("src", "images/reimu.png")
-        myImage.setAttribute("alt", "Osana Reimu eating.")
+        image_element.setAttribute("src", "images/reimu.png")
+        image_element.setAttribute("alt", "Osana Reimu eating.")
     }
 }
 
-let myButton = document.querySelector("button")
-let myH1 = document.querySelector("h1")
-
-// sets the user name for display on the title
-setUserName = () => {
+setUserName = (heading) => {
     const myName = prompt("Please enter your name.")
     if (!myName) {
         setUserName()
     } else {
         localStorage.setItem("name", myName)
-        myH1.textContent = `Web dev is cool, ${myName}`
+        heading.textContent = `Web dev is cool, ${myName}`
     }
 }
 
 // if the user name hasn't been set, set it
-if (!localStorage.getItem("name")) {
-    setUserName()
-} else {
-    const storedName = localStorage.getItem("name")
-    myH1.textContent = `Web dev is cool, ${storedName}`
+checkUserIsSet = (heading) => {
+    if (!localStorage.getItem("name")) {
+        setUserName(heading)
+    } else {
+        const storedName = localStorage.getItem("name")
+        heading.textContent = `Web dev is cool, ${storedName}`
+    }
 }
 
-myButton.onclick = () => {setUserName()}
+//Change image by clicking it
+const myImage = document.querySelector("img")
+myImage.onclick = () => {changeImage(myImage)}
+
+// sets the user name for display on the title
+let myButton = document.querySelector("button")
+let myH1 = document.querySelector("h1")
+myButton.onclick = () => {setUserName(myH1)}
+
+checkUserIsSet(myH1)
