646b3e796f99cce0f803bb2d2183f75ccaa5ff97
Omar Valdez <omarantoniovaldezf2@gmail.com>
2023-07-18 18:15:39 -0700

    Asynchronous programming finished

--- JS/async/animations/main.js
+++ JS/async/animations/main.js
@@ -12,3 +12,45 @@ const aliceTiming = {
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
+
+
+// Animation using callbacks (callback hell)
+function callbackHell() {
+  const promise1 = alice1.animate(aliceTumbling, aliceTiming).finished
+  promise1.then(_ => {
+    console.log('First Alice')
+    const promise2 = alice2.animate(aliceTumbling, aliceTiming).finished
+    
+    promise2.then(_ => {
+      console.log('Second Alice')
+      const promise3 = alice3.animate(aliceTumbling, aliceTiming).finished
+      
+      promise3.then(_ => {
+        console.log('Animations finished.')
+      })
+    })
+  })
+}
+
+// callbackHell()
+
+
+// Animation using a 'promised chain'
+function promisedChain() {
+  alice1.animate(aliceTumbling, aliceTiming).finished
+    .then(_ => alice2.animate(aliceTumbling, aliceTiming).finished)
+    .then(_ => alice3.animate(aliceTumbling, aliceTiming))
+}
+
+// promisedChain()
+
+
+// Animation using async and await
+async function asyncAwait() {
+  await alice1.animate(aliceTumbling, aliceTiming).finished
+  await alice2.animate(aliceTumbling, aliceTiming).finished
+  await alice3.animate(aliceTumbling, aliceTiming).finished
+  console.log('Animations finished (Using async and await).')
+}
+
+asyncAwait()
